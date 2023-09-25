bf447d902075904cd7f50853e605bf8a97605499
Pietro AS <pietroas123@gmail.com>
2023-09-24 23:26:53 -0300

    adapted to my Information and links

--- script.js
+++ script.js
@@ -8,17 +8,17 @@ function toggleMode() {
   //alterar imagem
   if (html.classList.contains("light")) {
     ///se for light, imagem 1
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/pi-light2.jpg")
     img.setAttribute(
       "alt",
-      "Imagem de usuario, Mayk brito. Com barba, oculos escuros e jaqueta preta. Fundo colorido"
+      "Imagem de usuario, Pietro. Com barba, blusa vermelha. ao céu aberto"
     )
   } else {
     ///se não, imagem 2
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/pi-dark2.jpg")
     img.setAttribute(
       "alt",
-      "Imagem de usuario, Mayk brito. Com barba, oculos e camiseta preta. Fundo amarelo"
+      "Imagem de usuario, Pietro. Com barba, blusa preta. Fundo decorado com luzes"
     )
   }
 }
